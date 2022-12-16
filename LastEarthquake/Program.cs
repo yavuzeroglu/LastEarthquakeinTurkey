7fda630d02b315928174f4dc44c65d74bed13c8f
Yavuz Eroğlu <106495712+yavuzeroglu@users.noreply.github.com>
2022-12-17 02:45:56 +0300

    Add project files.

---
+++ LastEarthquake/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+
+namespace LastEarthquake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string PullData(string url, string tag)
+            {
+                var startTag = "<" + tag + ">";
+                int startIndex = url.IndexOf(startTag) + startTag.Length;
+                int endIndex = url.IndexOf("</" + tag + ">", startIndex);
+                return url.Substring(startIndex, endIndex - startIndex);
+            }
+
+            string bounUrl;
+            using (WebClient client = new WebClient())
+                bounUrl = client.DownloadString("http://www.koeri.boun.edu.tr/scripts/lst4.asp");
+            
+
+            string result = PullData(bounUrl, "pre");
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
